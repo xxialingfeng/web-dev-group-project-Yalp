c51656a8b8d29d76be9a45faf42d51d375d3f4c7
Zhiyan <pei.zhiy@northeastern.edu>
2023-12-09 19:40:37 -0800

    submit 12/9 2nd

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route, Navigate } from "react-router-dom";
 import YalpNavigation from "./Yalp/YalpNavigation";
+import { Restaurant } from "./Yalp/Restaurants";
+import Yalp from "./Yalp";
 import Home from "./Yalp/Home";
 import Search from "./Yalp/search";
 import Details from "./Yalp/details";
